19ed4d293d9bdfc59c5d953f2ab952dbc1b2cb8b
Rafael de Melo <rafael5665@hotmail.com>
2022-02-04 01:13:40 -0300

    GET schedule

--- server/app.py
+++ server/app.py
@@ -25,76 +25,76 @@ def teste():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        try:
-            data = request.json
-            user = data['user']
-            password = data['password']
-            with sqlite3.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute("SELECT * FROM users WHERE user=?", (user,))
-                user_db = cur.fetchone()
-                if user_db[1] == password:
-                    msg = 'User logged.'
-                    login = True
-                else:
-                    msg = 'User or password incorrect.'
-                    login = False
-        except Exception as e:
-            con.rollback()
-            msg = f"Error trying to log user. Error: {e}"
+    con = sqlite3.connect("database.db")
+    try:
+        data = request.json
+        user = data['user']
+        password = data['password']
+        cur = con.cursor()
+        cur.execute("SELECT * FROM users WHERE user=?", (user,))
+        user_db = cur.fetchone()
+        if user_db[1] == password:
+            msg = 'User logged.'
+            login = True
+        else:
+            msg = 'User or password incorrect.'
             login = False
-        finally:
-            con.close()
-            return {
-                'user': user_db,
-                'msg': msg,
-                'login': login}, 201
+    except Exception as e:
+        con.rollback()
+        msg = f"Error trying to log user. Error: {e}"
+        login = False
+    finally:
+        con.close()
+        return {
+            'user': user_db,
+            'msg': msg,
+            'login': login}, 201
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        try:
-            data = request.json
-            user = data['user']
-            password = data['password']
-            key_user = data['key']
-            json = request.json
-            print(json)
+    con = sqlite3.connect("database.db")
+    try:
+        data = request.json
+        user = data['user']
+        password = data['password']
+        key_user = data['key']
+        json = request.json
+        print(json)
 
-            with sqlite3.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO users(user, password, key_user) \
-                    VALUES(?, ?, ?)", (user, password, key_user))
-                con.commit()
-                msg = 'User created!'
-        except Exception as e:
-            con.rollback()
-            msg = f"Error while. Error: {e}"
-        finally:
-            con.close()
-            return {'msg': msg}
+        cur = con.cursor()
+        cur.execute("INSERT INTO users(user, password, key_user) \
+            VALUES(?, ?, ?)", (user, password, key_user))
+        con.commit()
+        msg = 'User created!'
+    except Exception as e:
+        con.rollback()
+        msg = f"Error while. Error: {e}"
+    finally:
+        con.close()
+        return {'msg': msg}
 
 
 @app.route('/booking', methods=['GET','POST'])
 def booking():
+    con = sqlite3.connect("database.db")
     if request.method == 'POST':
         try:
             data = request.json
             key_user = data['key']
             data_request = data['data_request']
-            time_request = data['time_request']
-
-            with sqlite3.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute('SELECT COUNT(*) FROM booking_request')
-                id = cur.fetchone()
-                id = str((int(id[0])+1))
-                cur.execute('INSERT INTO booking_request(num_request, key_user, date_request, time_request, \
-                accepted) VALUES (?,?,?,?,0)', (id,key_user,data_request,time_request))
-                con.commit()
-                msg = 'Date booked!'
+            start_time_request = data['start_time_request']
+            end_time_request = data['end_time_request']
+            cur = con.cursor()
+            cur.execute('SELECT COUNT(*) FROM booking_request')
+            id = cur.fetchone()
+            id = str((int(id[0])+1))
+            cur.execute('INSERT INTO booking_request(num_request, key_user, date_request, \
+            start_time_request, end_time_request,accepted) VALUES (?,?,?,?,?,0)', 
+            (id,key_user,data_request,start_time_request,end_time_request))
+            print('Erro3')
+            con.commit()
+            msg = 'Date booked!'
         except Exception as e:
             con.rollback()
             msg = f"Error while. Error: {e}"
@@ -105,15 +105,13 @@ def booking():
         try:
             data = request.json
             num_request = data['num_request']
-
-            with sqlite3.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute("SELECT * FROM booking_request WHERE num_request=?", (num_request,))
-                booking_request = cur.fetchone()
-                if booking_request:
-                    msg = 'Request found'
-                else:
-                    msg = 'Request not found'
+            cur = con.cursor()
+            cur.execute("SELECT * FROM booking_request WHERE num_request=?", (num_request,))
+            booking_request = cur.fetchone()
+            if booking_request:
+                msg = 'Request found'
+            else:
+                msg = 'Request not found'
         except Exception as e:
             con.rollback()
             msg = f"Error while. Error: {e}"
@@ -122,6 +120,23 @@ def booking():
             return {
                 'msg': msg, 
                 'request':booking_request}
+
+@app.route('/schedule', methods=['GET'])
+def schedule():
+    con = sqlite3.connect("database.db")
+    try:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM booking_request")
+        booking_list = cur.fetchall()
+        msg = 'Schedule list.'
+    except Exception as e:
+        con.rollback()
+        msg = f"Error while. Error: {e}"
+    finally:
+        con.close()
+        return {
+            'msg': msg, 
+            'request':booking_list}
 
 
 if __name__ == '__main__':
